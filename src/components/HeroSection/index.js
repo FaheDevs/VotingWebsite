c28dacaa312fe9622999f87984555238fda4e66e
zidanifahed <fahed-imed.zidani@etu.yniv-amu.fr>
2022-05-26 23:51:44 +0200

    initial commit

---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import { Button } from '../ButtonElements';
+
+import Video from '../../videos/video.mp4';
+import {
+  HeroContainer,
+  HeroBg,
+  VideoBg,
+  HeroContent,
+  HeroH1,
+  HeroP,
+  HeroBtnWrapper,
+  ArrowForward,
+  ArrowRight
+} from './HeroElements';
+
+function HeroSection() {
+  const [hover, setHover] = useState(false);
+
+  const onHover = () => {
+    setHover(!hover);
+  };
+  return (
+    <HeroContainer id='home'>
+      <HeroBg>
+        <VideoBg playsInline autoPlay loop muted src={Video} type='video/mp4' />
+      </HeroBg>
+      <HeroContent>
+        <HeroH1>La solution électorale parfaite </HeroH1>
+        <HeroP>
+          i-voting est une plateforme de vote en ligne qui vvous aide a gérer vos élections facilement et en toute sécurité grace a la technologie de la blockchain
+        </HeroP>
+        <HeroBtnWrapper>
+          <Button
+            to='signup'
+            smooth={true}
+            duration={500}
+            spy={true}
+            exact='true'
+            offset={-80}
+            primary='true'
+            dark='true'
+            onMouseEnter={onHover}
+            onMouseLeave={onHover}
+          >
+            Administrateur  {hover ? <ArrowForward /> : <ArrowRight />}
+          </Button>
+        </HeroBtnWrapper>
+        <HeroBtnWrapper>
+          <Button
+            to='signup'
+            smooth={true}
+            duration={500}
+            spy={true}
+            exact='true'
+            offset={-80}
+            primary='true'
+            dark='true'
+            onMouseEnter={onHover}
+            onMouseLeave={onHover}
+          >
+            Electeur  {hover ? <ArrowForward /> : <ArrowRight />}
+          </Button>
+        </HeroBtnWrapper>
+      </HeroContent>
+    </HeroContainer>
+  );
+}
+
+export default HeroSection;
